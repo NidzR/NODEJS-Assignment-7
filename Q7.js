a219a480ed967ff65a2608e05410c688e336beba
Nida Rizwan <143074620+NidzR@users.noreply.github.com>
2024-01-14 02:47:11 +0500

    NODEJS Assignment Q7
    7. Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.

---
+++ Q7.js
@@ -0,0 +1,8 @@
+"use strict";
+// 7. Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+let a = 10;
+let b = 5;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
---
+++ Q7.ts
@@ -0,0 +1,8 @@
+// 7. Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+
+let a:number = 10;
+let b:number = 5;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
